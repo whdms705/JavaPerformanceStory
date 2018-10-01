1908d90cb44ac0b93cf2dff465b54e31724d9420
whdms705 <whdms705@nate.com>
2018-10-02 02:25:59 +0900

    Create anotation example

---
+++ src/ch07/anotion/MyObject.java
@@ -0,0 +1,22 @@
+package ch07.anotion;
+
+public class MyObject {
+    @StringInjector("My name is JDM.")
+    private String name;
+
+    @StringInjector
+    private String defaultValue;
+
+    @StringInjector
+    private Integer invalidType;
+
+    public String getName() {
+        return name;
+    }
+    public String getDefaultValue() {
+        return defaultValue;
+    }
+    public Integer getInvalidType() {
+        return invalidType;
+    }
+}
